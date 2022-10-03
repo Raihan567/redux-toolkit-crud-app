d2d24c15faa3780e273da8ec4dc8b30754fa4b54
Raihan567 <aburaihansarker567@gmail.com>
2022-10-03 16:08:48 +0600

    set provider with store value

--- src/features/ListView.jsx
+++ src/features/ListView.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const ListView = () => {
+  const books = useSelector((state) => state.bookReducer.book);
+  console.log(books);
   return (
     <div>
       <h1>Books of list</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>text1.1</td>
+            <td>text1.2</td>
+            <td>text1.3</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
--- src/store.js
+++ src/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import BookReducer from "./features/BookSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    bookReducer: BookReducer
+  },
 });
 
 export default store;
