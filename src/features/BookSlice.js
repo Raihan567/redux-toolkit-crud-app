7d0d6707d0c93f3bfe3bd68b8e52218fa54966b2
Raihan567 <aburaihansarker567@gmail.com>
2022-10-03 16:03:45 +0600

    create bookSlice reducer

---
+++ src/features/BookSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialBooks = [
+  {
+    id: 1,
+    title: "Self motivated",
+    author: "Abu Raihan",
+  },
+  {
+    id: 2,
+    title: "Learners Guide",
+    author: "Abu Raihan",
+  },
+  {
+    id: 3,
+    title: "Motivated to improve performance",
+    author: "Abu Raihan",
+  },
+];
+
+const BookSlice = createSlice({
+  name: "books",
+  initialState: initialBooks,
+  reducers: {
+    addBook: (state) => state,
+  },
+});
+
+export const { addBook } = BookSlice.actions;
+export default BookSlice.reducer;
